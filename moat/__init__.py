d63126e0b3a52822a0fb556c9d28f903e8d394b0
Andy Mroczkowski <andy@mrox.net>
2012-02-21 23:16:13 -0800

    packaging stuff

---
+++ moat/__init__.py
@@ -0,0 +1,10 @@
+"""django-moat"""
+
+VERSION = (0, 1, 0)
+
+__version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
+__author__ = "Andy Mroczkowski"
+__contact__ = "andy@mrox.net"
+__homepage__ = "http://amrox.github.com/django-moat/"
+__docformat__ = "restructuredtext"
+# __license__ = "BSD (3 clause)"
